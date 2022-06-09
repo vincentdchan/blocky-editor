af8c0a514d183846ed1d7c69542509f171d1b405
Vincent Chan <okcdz@diverse.space>
2022-06-09 20:55:10 +0800

    feat: build with code splitting

--- packages/blocky-core/rollup.config.js
+++ packages/blocky-core/rollup.config.js
@@ -4,13 +4,13 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: {
     'common/dom/index': "src/common/dom/index.ts",
-    'common/index': "src/common/index.ts"
+    'common/index': "src/common/index.ts",
+    'model/index': "src/model/index.ts"
   },
   output: {
     dir: 'dist',
     format: 'es'
   },
-  experimentalCodeSplitting: true,
   plugins: [typescript({
     declaration: true,
     declarationDir: "dist",
