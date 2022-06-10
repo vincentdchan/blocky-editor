1f3970f3dce84149caf906c5f06b78f6852fda12
Vincent Chan <okcdz@diverse.space>
2022-06-10 21:37:50 +0800

    feat: compile preact plugin with tsc

--- packages/blocky-preact/src/index.tsx
+++ packages/blocky-preact/src/index.tsx
@@ -1,15 +1,32 @@
-import { Component, JSX } from "preact";
+import { Component, createRef, type RefObject } from "preact";
+import { Editor, type IEditorOptions } from "blocky-core";
 
 interface Props {
-	name: string;
+  options: IEditorOptions;
 }
 
 export class BlockyEditor extends Component<Props> {
 
-	render() {
-		return (
-			<div></div>
-		);
-	}
+  #editor: Editor | undefined;
+  #containerRef: RefObject<HTMLDivElement> = createRef();
+
+  override componentDidMount() {
+    const { options } = this.props;
+    this.#editor = new Editor({
+      ...options,
+      container: this.#containerRef.current!,
+    });
+  }
+
+  override componentWillUnmount() {
+    this.#editor?.dispose();
+    this.#editor = undefined;
+  }
+
+  render() {
+    return (
+      <div ref={this.#containerRef}></div>
+    );
+  }
 
 }
