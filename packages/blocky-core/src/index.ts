ff7de81fa873e7920087d0bad96be8c08813e269
Vincent Chan <okcdz@diverse.space>
2022-06-11 23:11:05 +0800

    feat: add editor controller

--- packages/blocky-core/src/index.ts
+++ packages/blocky-core/src/index.ts
@@ -1,5 +1,6 @@
 
 export * from "./view/editor";
 export * as markup from "./model/markup";
+export * from "./view/controller";
 export { makeDefaultIdGenerator, type IdGenerator } from "./helper/idHelper";
 export { default as DocumentState } from "./model/state";
