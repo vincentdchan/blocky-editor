e51e3fc130a846c8b5864cc9f08dd98df01b1aa9
Vincent Chan <okcdz@diverse.space>
2022-06-12 18:09:36 +0800

    feat: support heading

--- packages/blocky-core/src/block/textBlock.ts
+++ packages/blocky-core/src/block/textBlock.ts
@@ -3,10 +3,16 @@ import { type IBlockDefinition, BlockContentType, type SpanCreatedEvent } from "
 
 export const TextBlockName = "text";
 
+export interface ITextBlockOptions {
+  level?: number;
+}
+
 class TextBlockImpl implements IBlockDefinition {
 
   public name: string = TextBlockName;
   public type: BlockContentType = BlockContentType.Text;
+
+  constructor(private options?: ITextBlockOptions) {}
 
   findContentContainer(parent: HTMLElement) {
     return parent.firstChild! as HTMLElement;
@@ -14,11 +20,21 @@ class TextBlockImpl implements IBlockDefinition {
 
   onContainerCreated({ element, clsPrefix }: SpanCreatedEvent) {
     const content = elem("div", `${clsPrefix}-line-content`);
+
+    const level = this.options?.level ?? 0;
+    if (level === 1) {
+      content.classList.add("blocky-heading1");
+    } else if (level === 2) {
+      content.classList.add("blocky-heading2");
+    } else if (level === 3) {
+      content.classList.add("blocky-heading3");
+    }
+
     element.appendChild(content);
   }
 
 }
 
-export function makeTextBlockDefinition(): IBlockDefinition {
-  return new TextBlockImpl;
+export function makeTextBlockDefinition(options?: ITextBlockOptions): IBlockDefinition {
+  return new TextBlockImpl(options);
 }
