5639f7125eca87130244b52a920e1104b205076e
Vincent Chan <okcdz@diverse.space>
2022-06-16 22:41:39 +0800

    refactor: handle format text in controller

--- packages/blocky-core/src/plugins/boldedTextPlugin.ts
+++ packages/blocky-core/src/plugins/boldedTextPlugin.ts
@@ -1,47 +1,18 @@
 import type { IPlugin } from "@pkg/registry/pluginRegistry";
-import { type TreeNode, type DocNode, type BlockData } from "@pkg/model";
 import type { Editor } from "@pkg/view/editor";
-import { type Action } from "@pkg/model/actions";
 import { isHotkey } from "is-hotkey";
 import { AttributesObject } from "@pkg/model/textModel";
 
 /**
  * This plugin is used to make the editor support bolded text.
- * 
+ *
  * The bolded text will be wrapped in as `<span>` element.
  * So the plugin register the span in the editor's `SpanRegistry`.
- * 
+ *
  * After all, this plugin handles the hotkey to make selected text bolded.
- * 
+ *
  */
 function makeBoldedTextPlugin(): IPlugin {
-  const makeTextBlockRangeBolded = (
-    editor: Editor,
-    blockNode: TreeNode<DocNode>,
-    startOffset: number,
-    endOffset: number
-  ) => {
-    if (startOffset === endOffset) {
-      return;
-    }
-
-    const block = blockNode.data as BlockData;
-
-    const actions: Action[] = [
-      {
-        type: "text-format",
-        targetId: block.id,
-        index: startOffset,
-        length: endOffset - startOffset,
-        attributes: {
-          bold: true
-        },
-      },
-    ];
-
-    editor.applyActions(actions);
-    editor.render();
-  };
   const makeBold = (editor: Editor) => {
     const { cursorState } = editor.state;
     if (!cursorState) {
@@ -65,7 +36,14 @@ function makeBoldedTextPlugin(): IPlugin {
         console.error(`${startId} is not a block`);
         return;
       }
-      makeTextBlockRangeBolded(editor, blockNode, startOffset, endOffset);
+      editor.controller.formatText(
+        blockNode.data.id,
+        startOffset,
+        endOffset - startOffset,
+        {
+          bold: true,
+        }
+      );
     } else {
       console.log("unimplemented bold");
     }
@@ -73,11 +51,13 @@ function makeBoldedTextPlugin(): IPlugin {
   return {
     name: "bolded-text",
     onInitialized(editor: Editor) {
-      editor.registry.span.on((element: HTMLSpanElement, attribs: AttributesObject) => {
-        if (attribs.bold === true) {
-          element.classList.add("mg-editor-bold");
+      editor.registry.span.on(
+        (element: HTMLSpanElement, attribs: AttributesObject) => {
+          if (attribs.bold === true) {
+            element.classList.add("mg-editor-bold");
+          }
         }
-      });
+      );
       editor.keyDown.on((e: KeyboardEvent) => {
         if (isHotkey("mod+b", e)) {
           e.preventDefault();
