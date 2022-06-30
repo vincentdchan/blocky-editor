c91dbf759af4babb323bb6e51b0c830da5d35452
Vincent Chan <okcdz@diverse.space>
2022-06-30 20:39:55 +0800

    feat: add collaborativeCursors

--- packages/blocky-core/src/model/cursor.ts
+++ packages/blocky-core/src/model/cursor.ts
@@ -1,17 +1,16 @@
 
 export interface CollapsedCursor {
-    type: "collapsed";
-    targetId: string;
-    offset: number;
-  }
-  
-  export interface OpenCursorState {
-    type: "open";
-    startId: string;
-    startOffset: number;
-    endId: string;
-    endOffset: number;
-  }
-  
-  export type CursorState = CollapsedCursor | OpenCursorState;
-  
+  type: "collapsed";
+  targetId: string;
+  offset: number;
+}
+
+export interface OpenCursorState {
+  type: "open";
+  startId: string;
+  startOffset: number;
+  endId: string;
+  endOffset: number;
+}
+
+export type CursorState = CollapsedCursor | OpenCursorState;
