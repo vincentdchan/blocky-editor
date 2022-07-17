08fff404c697ce5c32a3ac345354918831463128
Vincent Chan <okcdz@diverse.space>
2022-07-17 10:15:02 +0800

    refactor: separate events

---
+++ packages/blocky-core/src/model/events.ts
@@ -0,0 +1,52 @@
+import type { AttributesObject, BlockyNode } from "./element";
+
+export interface TextInsertEvent {
+  type: "text-insert";
+  index: number;
+  text: string;
+  attributes?: AttributesObject;
+}
+
+export interface TextDeleteEvent {
+  type: "text-delete";
+  index: number;
+  length: number;
+}
+
+export interface TextFormatEvent {
+  type: "text-format";
+  index: number;
+  length: number;
+  attributes?: AttributesObject;
+}
+
+export type TextChangedEvent =
+  | TextInsertEvent
+  | TextDeleteEvent
+  | TextFormatEvent;
+
+export interface ElementSetAttributeEvent {
+  type: "element-set-attrib";
+  key: string;
+  value: string;
+  oldValue?: string;
+}
+
+export interface ElementRemoveChildEvent {
+  type: "element-remove-child";
+  parent: BlockyNode;
+  child: BlockyNode;
+  getInsertIndex: () => number;
+}
+
+export interface ElementInsertChildEvent {
+  type: "element-insert-child";
+  parent: BlockyNode;
+  child: BlockyNode;
+  getInsertIndex: () => number;
+}
+
+export type ElementChangedEvent =
+  | ElementSetAttributeEvent
+  | ElementInsertChildEvent
+  | ElementRemoveChildEvent;
