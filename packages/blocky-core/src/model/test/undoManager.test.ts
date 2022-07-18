9dee82aea2d579df58b3993be540e1c9e127319f
Vincent Chan <okcdz@diverse.space>
2022-07-18 14:06:36 +0800

    feat: add operations of undoManager

--- packages/blocky-core/src/model/test/undoManager.test.ts
+++ packages/blocky-core/src/model/test/undoManager.test.ts
@@ -1,27 +1,38 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import { FixedSizeStack, StackItem } from "../undoManager";
 
-test("stack push() and pop()", () => {
-  const fixedSizeStack = new FixedSizeStack(100);
-  expect(fixedSizeStack.length).toBe(0);
-  const first = new StackItem();
-  fixedSizeStack.push(first);
-  expect(fixedSizeStack.length).toBe(1);
-  const poped = fixedSizeStack.pop();
-  expect(poped).toBe(first);
-  expect(fixedSizeStack.pop()).toBeUndefined();
-});
+describe("FixedSizeStack", () => {
+  test("push() + pop()", () => {
+    const fixedSizeStack = new FixedSizeStack(100);
+    expect(fixedSizeStack.length).toBe(0);
+    const first = new StackItem();
+    fixedSizeStack.push(first);
+    expect(fixedSizeStack.length).toBe(1);
+    const poped = fixedSizeStack.pop();
+    expect(poped).toBe(first);
+    expect(fixedSizeStack.pop()).toBeUndefined();
+  });
 
-test("maxSize of stack", () => {
-  const fixedSizeStack = new FixedSizeStack(2);
-  const first = new StackItem();
-  fixedSizeStack.push(first);
-  expect(fixedSizeStack.length).toBe(1);
-  const second = new StackItem();
-  fixedSizeStack.push(second);
-  expect(fixedSizeStack.length).toBe(2);
-  const third = new StackItem();
-  fixedSizeStack.push(third);
-  expect(fixedSizeStack.length).toBe(2);
-  expect(fixedSizeStack.peek()).toBe(third);
+  test("maxSize", () => {
+    const fixedSizeStack = new FixedSizeStack(2);
+    const first = new StackItem();
+    fixedSizeStack.push(first);
+    expect(fixedSizeStack.length).toBe(1);
+    const second = new StackItem();
+    fixedSizeStack.push(second);
+    expect(fixedSizeStack.length).toBe(2);
+    const third = new StackItem();
+    fixedSizeStack.push(third);
+    expect(fixedSizeStack.length).toBe(2);
+    expect(fixedSizeStack.peek()).toBe(third);
+  });
+
+  test("clear()", () => {
+    const fixedSizeStack = new FixedSizeStack(10);
+    fixedSizeStack.push(new StackItem());
+    fixedSizeStack.push(new StackItem());
+    expect(fixedSizeStack.length).toBe(2);
+    fixedSizeStack.clear();
+    expect(fixedSizeStack.length).toBe(0);
+  });
 });
