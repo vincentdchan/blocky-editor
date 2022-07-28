70a08df861e8b5163d372059dce7482c0da11f9d
Vincent Chan <okcdz@diverse.space>
2022-07-28 13:30:44 +0800

    feat: transform text edit

---
+++ packages/blocky-core/src/model/versionHistory.ts
@@ -0,0 +1,21 @@
+import { FinalizedChangeset } from "./change";
+
+export class VersionHistory {
+  #baseVersion: number;
+  #data: FinalizedChangeset[] = [];
+
+  constructor(baseVersion = 0) {
+    this.#baseVersion = baseVersion;
+  }
+  get(index: number): FinalizedChangeset | undefined {
+    index -= this.#baseVersion;
+    return this.#data[index];
+  }
+  insert(changeset: FinalizedChangeset) {
+    const index = changeset.version;
+    if (index < this.#baseVersion) {
+      throw new Error(`version ${index} is missing`);
+    }
+    this.#data[index] = changeset;
+  }
+}
