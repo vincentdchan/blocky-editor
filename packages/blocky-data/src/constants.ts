35f90aba1d495badaf17651987bcec13721cc75c
Vincent Chan <okcdz@diverse.space>
2022-09-04 19:08:13 +0800

    feat: use string as textType

--- packages/blocky-data/src/constants.ts
+++ packages/blocky-data/src/constants.ts
@@ -1,8 +1,30 @@
 export enum TextType {
-  Checkbox = -2,
-  Bulleted = -1,
-  Normal = 0,
-  Heading1 = 1,
-  Heading2 = 2,
-  Heading3 = 3,
+  Checkbox = "checkbox",
+  Bulleted = "bulleted",
+  Normal = "normal",
+  Heading1 = "h1",
+  Heading2 = "h2",
+  Heading3 = "h3",
+}
+
+export function textTypePrecedence(textType: TextType): number {
+  switch (textType) {
+    case TextType.Checkbox:
+      return -2;
+
+    case TextType.Bulleted:
+      return -1;
+
+    case TextType.Heading1:
+      return 1;
+
+    case TextType.Heading2:
+      return 2;
+
+    case TextType.Heading3:
+      return 3;
+
+    default:
+      return 0;
+  }
 }
