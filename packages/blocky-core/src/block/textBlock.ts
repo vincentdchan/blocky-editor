013cac941ccbe9b3e653fbd29d6f34b503526dab
Vincent Chan <okcdz@diverse.space>
2023-11-17 10:42:59 +0800

    feat: add render option

--- packages/blocky-core/src/block/textBlock.ts
+++ packages/blocky-core/src/block/textBlock.ts
@@ -27,6 +27,7 @@ import { EditorController } from "@pkg/view/controller";
 import type { SpanStyle } from "@pkg/registry/spanRegistry";
 import type { Embed } from "@pkg/registry/embedRegistry";
 import { Subject, fromEvent, takeUntil } from "rxjs";
+import { RenderOption } from "@pkg/view/renderer";
 
 const TextContentClass = "blocky-block-text-content";
 
@@ -459,7 +460,7 @@ export class TextBlock extends Block {
     return this.props.getAttribute<BlockyTextModel>("textContent")!;
   }
 
-  override render(container: HTMLElement) {
+  override render(container: HTMLElement, option: RenderOption) {
     this.#container = container;
 
     const textModel = this.textModel;
