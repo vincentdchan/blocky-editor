7ed42119d63fc5977a64f1e4fd8b06708aa3fda9
Vincent Chan <okcdz@diverse.space>
2023-11-21 01:06:25 +0800

    feat: add icons (#83)

--- packages/blocky-example/components/sidebar.tsx
+++ packages/blocky-example/components/sidebar.tsx
@@ -1,21 +1,35 @@
 import React, { memo } from "react";
 import Link from "next/link";
+import styles from "./sidebar.module.scss";
+import { IoHomeOutline } from "react-icons/io5";
+import { IoDocumentOutline } from "react-icons/io5";
+
+interface SidebarItemProps {
+  icon?: React.ReactNode;
+  href: string;
+  children?: React.ReactNode;
+}
+
+function SidebarItem(props: SidebarItemProps) {
+  const { icon, children, href } = props;
+  return (
+    <Link className={styles.item} href={href}>
+      {icon ?? <IoDocumentOutline />}
+      <div className={styles.content}>{children}</div>
+    </Link>
+  );
+}
 
 const Sidebar = memo(() => {
   return (
     <div className="blocky-example-sidebar-container">
-      <Link className="blocky-example-link" href="/">
+      <SidebarItem href="/" icon={<IoHomeOutline />}>
         Home
-      </Link>
-      <Link className="blocky-example-link" href="/doc/get-started">
-        Get started
-      </Link>
-      <Link className="blocky-example-link" href="/doc/api">
-        Api
-      </Link>
-      <Link className="blocky-example-link" href="/noTitle">
-        Editor without title
-      </Link>
+      </SidebarItem>
+      <SidebarItem href="/doc/get-started">Get started</SidebarItem>
+      <SidebarItem href="/doc/api">Api</SidebarItem>
+      <h2 className={styles.subTitle}>Examples</h2>
+      <SidebarItem href="/noTitle">Editor without title</SidebarItem>
     </div>
   );
 });
