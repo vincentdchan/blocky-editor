edaf7b59ab0c8ad8262fea41f36e6bac058feb9d
Vincent Chan <okcdz@diverse.space>
2023-11-20 20:17:42 +0800

    feat: adjust for SEO

--- packages/blocky-example/app/layout.tsx
+++ packages/blocky-example/app/layout.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Script from "next/script";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blocky Editor Example",
+};
 
 export default function RootLayout({
   children,
@@ -7,6 +13,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PS6NK1TQ06"
+        ></Script>
+        <Script>
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PS6NK1TQ06');
+    `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
