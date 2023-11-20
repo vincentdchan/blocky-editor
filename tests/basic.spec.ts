66ffea4e9f622cb92ac363a1f49a56010bdd8d65
Vincent Chan <okcdz@diverse.space>
2023-11-20 21:06:43 +0800

    feat: update config of playwright

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("can open example page", async ({ page }) => {
-  await page.goto("http://localhost:9000/");
+  await page.goto("http://localhost:3000/");
   await expect(page).toHaveTitle(/Blocky Editor Example/);
 });
