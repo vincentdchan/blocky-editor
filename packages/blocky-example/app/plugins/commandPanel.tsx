d151dc31acde4cb4acb3f24b6374fd8321ea93e8
Vincent Chan <okcdz@diverse.space>
2023-11-25 11:26:39 +0800

    feat: command panel

--- packages/blocky-example/app/plugins/commandPanel.tsx
+++ packages/blocky-example/app/plugins/commandPanel.tsx
@@ -1,6 +1,5 @@
-import { PureComponent } from "react";
+import { memo, useCallback } from "react";
 import { makeReactFollowerWidget } from "blocky-react";
-import { type IDisposable, flattenDisposable } from "blocky-common/es";
 import {
   Panel,
   PanelItem,
@@ -23,50 +22,40 @@ interface CommandPanelProps {
 
 const commandsLength = 1;
 
-class CommandPanel extends PureComponent<CommandPanelProps> {
-  private disposables: IDisposable[] = [];
-  constructor(props: CommandPanelProps) {
-    super(props);
-  }
+const CommandPanel = memo((props: CommandPanelProps) => {
+  const { editingValue, closeWidget } = props;
 
-  #handleSelect = (selectedIndex: number) => {
+  const handleSelect = useCallback((selectedIndex: number) => {
     if (selectedIndex === 0) {
       alert("Command");
     }
-  };
+  }, []);
 
-  #handleClose = () => {
-    this.props.closeWidget();
-  };
+  const handleClose = useCallback(() => {
+    closeWidget();
+  }, [closeWidget]);
 
-  override componentWillUnmount() {
-    flattenDisposable(this.disposables).dispose();
-  }
-
-  render() {
-    const { editingValue } = this.props;
-    const commandContent = editingValue;
-    return (
-      <SelectablePanel
-        onSelect={this.#handleSelect}
-        onClose={this.#handleClose}
-        controller={this.props.controller}
-        length={commandsLength}
-      >
-        {(index: number) => (
-          <Panel>
-            <PanelValue>
-              Command: {commandContent.length === 0 ? "Empty" : commandContent}
-            </PanelValue>
-            <div className="blocky-commands-container">
-              <PanelItem selected={index === 0}>Alert</PanelItem>
-            </div>
-          </Panel>
-        )}
-      </SelectablePanel>
-    );
-  }
-}
+  const commandContent = editingValue;
+  return (
+    <SelectablePanel
+      onSelect={handleSelect}
+      onClose={handleClose}
+      controller={props.controller}
+      length={commandsLength}
+    >
+      {(index: number) => (
+        <Panel>
+          <PanelValue>
+            Command: {commandContent.length === 0 ? "Empty" : commandContent}
+          </PanelValue>
+          <div className="blocky-commands-container">
+            <PanelItem selected={index === 0}>Alert</PanelItem>
+          </div>
+        </Panel>
+      )}
+    </SelectablePanel>
+  );
+});
 
 export function makeCommandPanelPlugin(): IPlugin {
   return {
