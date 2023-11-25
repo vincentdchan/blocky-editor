80e6f2bac85bfc51c54bbe7bc453eb94aff1b216
Vincent Chan <okcdz@diverse.space>
2023-11-25 12:05:29 +0800

    fix: remove unused vars

--- packages/blocky-example/app/themeSwitch.tsx
+++ packages/blocky-example/app/themeSwitch.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { Component, createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export interface ThemeContext {
   darkMode: boolean;
