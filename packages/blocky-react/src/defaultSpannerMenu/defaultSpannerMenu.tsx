32631050ea34012e0c2e711648ce885b5f7c953e
Vincent Chan <okcdz@diverse.space>
2023-11-27 09:45:09 +0800

    refactor: spanner menu

--- packages/blocky-react/src/defaultSpannerMenu/defaultSpannerMenu.tsx
+++ packages/blocky-react/src/defaultSpannerMenu/defaultSpannerMenu.tsx
@@ -1,108 +1,70 @@
-import { Component, type RefObject, createRef } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
+import ReactDOM from "react-dom";
 import { type EditorController, BlockDataElement, TextType } from "blocky-core";
 import Dropdown from "@pkg/components/dropdown";
 import { Menu, MenuItem, Divider } from "@pkg/components/menu";
 import { ImageBlockName } from "../";
 import { Subject, takeUntil } from "rxjs";
-import { css } from "@emotion/react";
+import { SpannerIcon, buttonStyle } from "./style";
 
 export interface SpannerProps {
   editorController: EditorController;
   focusedNode?: BlockDataElement;
 }
 
-interface SpannerState {
-  showDropdown: boolean;
-  menuX: number;
-  menuY: number;
-  showDelete: boolean;
+interface Coord {
+  x: number;
+  y: number;
 }
 
-const SpannerIcon = `
-<svg role="graphics-symbol" viewBox="0 0 10 10" class="dragHandle" style="width: 14px; height: 14px; display: block; fill: inherit; flex-shrink: 0;"><path d="M3,2 C2.44771525,2 2,1.55228475 2,1 C2,0.44771525 2.44771525,0 3,0 C3.55228475,0 4,0.44771525 4,1 C4,1.55228475 3.55228475,2 3,2 Z M3,6 C2.44771525,6 2,5.55228475 2,5 C2,4.44771525 2.44771525,4 3,4 C3.55228475,4 4,4.44771525 4,5 C4,5.55228475 3.55228475,6 3,6 Z M3,10 C2.44771525,10 2,9.55228475 2,9 C2,8.44771525 2.44771525,8 3,8 C3.55228475,8 4,8.44771525 4,9 C4,9.55228475 3.55228475,10 3,10 Z M7,2 C6.44771525,2 6,1.55228475 6,1 C6,0.44771525 6.44771525,0 7,0 C7.55228475,0 8,0.44771525 8,1 C8,1.55228475 7.55228475,2 7,2 Z M7,6 C6.44771525,6 6,5.55228475 6,5 C6,4.44771525 6.44771525,4 7,4 C7.55228475,4 8,4.44771525 8,5 C8,5.55228475 7.55228475,6 7,6 Z M7,10 C6.44771525,10 6,9.55228475 6,9 C6,8.44771525 6.44771525,8 7,8 C7.55228475,8 8,8.44771525 8,9 C8,9.55228475 7.55228475,10 7,10 Z"></path></svg>
-`;
+function SpannerMenu(props: SpannerProps) {
+  const { editorController, focusedNode } = props;
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [menuCoord, setMenuCoord] = useState<Coord>({ x: 0, y: 0 });
+  const bannerRef = useRef<HTMLDivElement>(null);
 
-const buttonStyle = css({
-  width: "16px",
-  height: "16px",
-  borderRadius: "4px",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  paddingTop: "3px",
-  paddingBottom: "3px",
-  "&:hover": {
-    cursor: "grab",
-    boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.2)",
-  },
-  svg: {
-    width: "100%",
-    height: "100%",
-  },
-});
+  useEffect(() => {
+    const dispose$ = new Subject<void>();
 
-class SpannerMenu extends Component<SpannerProps, SpannerState> {
-  private bannerRef: RefObject<HTMLDivElement> = createRef();
-  private dispose$ = new Subject<void>();
+    const handleBlocksChanged = () => {
+      const blockCount = editorController.state.blocks.size;
 
-  constructor(props: SpannerProps) {
-    super(props);
-    this.state = {
-      showDropdown: false,
-      menuX: 0,
-      menuY: 0,
-      showDelete: false,
+      const showDelete = blockCount > 1;
+      setShowDelete(showDelete);
     };
-  }
 
-  override componentDidMount() {
-    const { editorController } = this.props;
     const { state } = editorController;
     state.newBlockCreated
-      .pipe(takeUntil(this.dispose$))
-      .subscribe(this.handleBlocksChanged);
+      .pipe(takeUntil(dispose$))
+      .subscribe(handleBlocksChanged);
     state.blockWillDelete
-      .pipe(takeUntil(this.dispose$))
-      .subscribe(this.handleBlocksChanged);
+      .pipe(takeUntil(dispose$))
+      .subscribe(handleBlocksChanged);
 
-    this.handleBlocksChanged();
+    handleBlocksChanged();
 
-    this.bannerRef.current!.innerHTML = SpannerIcon;
-  }
+    bannerRef.current!.innerHTML = SpannerIcon;
 
-  override componentWillUnmount() {
-    this.dispose$.next();
-  }
+    return () => {
+      dispose$.next();
+      dispose$.complete();
+    };
+  }, [editorController]);
 
-  private handleBlocksChanged = () => {
-    const { editorController } = this.props;
-
-    const blockCount = editorController.state.blocks.size;
-
-    const showDelete = blockCount > 1;
-    if (showDelete === this.state.showDelete) {
-      return;
-    }
-    this.setState({ showDelete });
-  };
-
-  private handleClick = () => {
-    const rect = this.bannerRef.current!.getBoundingClientRect();
-    this.setState({
-      showDropdown: true,
-      menuX: rect.x,
-      menuY: rect.y,
+  const handleClick = useCallback(() => {
+    const rect = bannerRef.current!.getBoundingClientRect();
+    ReactDOM.unstable_batchedUpdates(() => {
+      setShowDropdown(true);
+      setMenuCoord({ x: rect.x, y: rect.y });
     });
-  };
+  }, []);
 
-  private handleMaskClicked = () => {
-    this.setState({
-      showDropdown: false,
-    });
-  };
+  const handleMaskClicked = useCallback(() => {
+    setShowDropdown(false);
+  }, []);
 
-  private insertText = (textType: TextType) => () => {
-    const { editorController, focusedNode } = this.props;
+  const insertText = (textType: TextType) => () => {
     if (!focusedNode) {
       return;
     }
@@ -114,8 +76,7 @@ class SpannerMenu extends Component<SpannerProps, SpannerState> {
     });
   };
 
-  private insertImage = () => {
-    const { editorController, focusedNode } = this.props;
+  const insertImage = () => {
     if (!focusedNode) {
       return;
     }
@@ -126,49 +87,38 @@ class SpannerMenu extends Component<SpannerProps, SpannerState> {
     });
   };
 
-  private deleteBlock = () => {
-    const { editorController, focusedNode } = this.props;
+  const deleteBlock = () => {
     if (!focusedNode) {
       return;
     }
     editorController.deleteBlock(focusedNode.id);
   };
 
-  private renderMenu() {
-    const { menuX, showDelete } = this.state;
-    let { menuY } = this.state;
-    menuY += 36;
+  const renderMenu = () => {
+    const menuY = menuCoord.y + 36;
     return (
       <Menu
         style={{
           position: "fixed",
-          left: `${menuX}px`,
+          left: `${menuCoord.x}px`,
           top: `${menuY}px`,
           ...{
-            ["--blocky-font"]: this.props.editorController.editor?.fontFamily,
+            ["--blocky-font"]: editorController.editor?.fontFamily,
           },
         }}
       >
-        <MenuItem onClick={this.insertText(TextType.Normal)}>Text</MenuItem>
-        <MenuItem onClick={this.insertText(TextType.Heading1)}>
-          Heading1
-        </MenuItem>
-        <MenuItem onClick={this.insertText(TextType.Heading2)}>
-          Heading2
-        </MenuItem>
-        <MenuItem onClick={this.insertText(TextType.Heading3)}>
-          Heading3
-        </MenuItem>
-        <MenuItem onClick={this.insertText(TextType.Checkbox)}>
-          Checkbox
-        </MenuItem>
-        <MenuItem onClick={this.insertImage}>Image</MenuItem>
+        <MenuItem onClick={insertText(TextType.Normal)}>Text</MenuItem>
+        <MenuItem onClick={insertText(TextType.Heading1)}>Heading1</MenuItem>
+        <MenuItem onClick={insertText(TextType.Heading2)}>Heading2</MenuItem>
+        <MenuItem onClick={insertText(TextType.Heading3)}>Heading3</MenuItem>
+        <MenuItem onClick={insertText(TextType.Checkbox)}>Checkbox</MenuItem>
+        <MenuItem onClick={insertImage}>Image</MenuItem>
         {showDelete && (
           <>
             <Divider />
             <MenuItem
               style={{ color: "var(--danger-color)" }}
-              onClick={this.deleteBlock}
+              onClick={deleteBlock}
             >
               Delete
             </MenuItem>
@@ -176,24 +126,17 @@ class SpannerMenu extends Component<SpannerProps, SpannerState> {
         )}
       </Menu>
     );
-  }
+  };
 
-  render() {
-    const { showDropdown } = this.state;
-    return (
-      <Dropdown
-        show={showDropdown}
-        overlay={this.renderMenu()}
-        onMaskClicked={this.handleMaskClicked}
-      >
-        <div
-          ref={this.bannerRef}
-          css={buttonStyle}
-          onClick={this.handleClick}
-        ></div>
-      </Dropdown>
-    );
-  }
+  return (
+    <Dropdown
+      show={showDropdown}
+      overlay={renderMenu()}
+      onMaskClicked={handleMaskClicked}
+    >
+      <div ref={bannerRef} css={buttonStyle} onClick={handleClick}></div>
+    </Dropdown>
+  );
 }
 
 export default SpannerMenu;
